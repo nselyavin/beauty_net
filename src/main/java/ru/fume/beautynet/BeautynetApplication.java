a8d2a74f052a23e9a88bd7027e2a2c453edb6e70
Nikita Selyavin <nselyavin@inbox.ru>
2021-04-27 19:45:38 +0300

    Project init

---
+++ src/main/java/ru/fume/beautynet/BeautynetApplication.java
@@ -0,0 +1,13 @@
+package ru.fume.beautynet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautynetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautynetApplication.class, args);
+	}
+
+}
